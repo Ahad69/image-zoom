53bee335092b9be1d6f37cadc9ed868f33bd5feb
Ahad hossain <ahadxx99@gmail.com>
2022-08-29 23:31:35 -0500

    image-zoom

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Zoom from "../components/zoom";
+
+export default () => (
+  <div className="zoom-container">
+    <h1>Image Zoom Example with Sloth</h1>
+    <Zoom
+      large="../static/sloth-large.jpg"
+      preview="../static/sloth-preview.jpg"
+    />
+    <style jsx>
+      {`
+        .zoom-container {
+          width: 600px;
+          margin: auto;
+        }
+      `}
+    </style>
+  </div>
+);
